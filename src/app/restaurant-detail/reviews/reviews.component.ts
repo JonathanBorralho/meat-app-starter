acadbc5074ce11825e7f31f2d84666305ad262b6
JonathanBorralho <jonathan.borralho@gmail.com>
2018-06-27 11:05:30 -0300

    Implementado Componente de Avaliações. Pipes Async e Date. Aula 50

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantsService } from './../../restaurants/restaurants.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'mt-reviews',
@@ -6,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews$: Observable<any>;
+  imageUrl = 'assets/img/reactions';
+
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id: string = this.route.parent.snapshot.params['id'];
+    this.reviews$ = this.restaurantsService.reviewsOfRestaurant(id);
+  }
+
+  reviewUrlImage(rating: number): string {
+    let imgName: string;
+
+    if (rating < 3) {
+      imgName = 'pissed';
+    } else if (rating >= 3 && rating < 4) {
+      imgName = 'soso';
+    } else if (rating >= 4) {
+      imgName = 'loved';
+    }
+
+    return `${this.imageUrl}/${imgName}.png`;
   }
 
 }
