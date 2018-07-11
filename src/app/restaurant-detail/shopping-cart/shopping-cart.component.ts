dbd98d5778a9bc04d37971250f206aa2e7bff649
JonathanBorralho <jonathan.borralho@gmail.com>
2018-07-11 16:32:14 -0300

    Animação com keyframe nos Itens do Carrinho. Aula 91

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,11 +1,30 @@
-import { MenuItem } from './../menu-item/menu-item.model';
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes } from '../../../../node_modules/@angular/animations';
+import { MenuItem } from './../menu-item/menu-item.model';
 import { ShoppingCartService } from './shopping-cart.service';
 import { CartItem } from './cart-item.model';
 
 @Component({
   selector: 'mt-shopping-cart',
-  templateUrl: './shopping-cart.component.html'
+  templateUrl: './shopping-cart.component.html',
+  animations: [
+    trigger('row', [
+      transition(':enter', [
+        animate('300ms 0s ease-in', keyframes([
+          style({ opacity: 0, transform: 'translateX(-30px)', offset: 0 }),
+          style({ opacity: 0.8, transform: 'translateX(10px)', offset: 0.8 }),
+          style({ opacity: 0, transform: 'translateX(0px)', offset: 1 })
+        ]))
+      ]),
+      transition(':leave', [
+        animate('300ms 0s ease-out', keyframes([
+          style({ opacity: 1, transform: 'translateX(0px)', offset: 0 }),
+          style({ opacity: 0.8, transform: 'translateX(-10px)', offset: 0.2 }),
+          style({ opacity: 0, transform: 'translateX(30px)', offset: 1 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class ShoppingCartComponent implements OnInit {
 
