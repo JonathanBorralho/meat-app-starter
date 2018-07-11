74d1551ee2a0e362a33c4083fa3e807e7ccf81c4
JonathanBorralho <jonathan.borralho@gmail.com>
2018-07-11 15:46:29 -0300

    Animações nos Itens do menu. Aula 89

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { trigger, style, transition, animate } from '../../../../node_modules/@angular/animations';
 import { MenuItem } from './menu-item.model';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      transition(':enter', [ // obs.: o tutor do curso usou o mesmo código da animação restaurantAppeared. Essa é uma versão reduzida
+        style({ opacity: 0, transform: 'translateY(-20px)'}),
+        animate('500ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
 
